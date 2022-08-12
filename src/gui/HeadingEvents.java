021c93bcd87d138c8743106f344931d47099cdc0
Fernando <fercu1@hotmail.com>
2022-08-11 19:56:48 -0500

    first commit

---
+++ src/gui/HeadingEvents.java
@@ -0,0 +1,15 @@
+package gui;
+
+import java.awt.event.ActionListener;
+
+public class HandLingEvents implements ActionListener {
+    public static final String ADD_DISH = "Agregar plato";
+    public static final String DELETE_DISH = "Borrar plato";
+    public static final String FIND_DISH = "Buscar plato";
+    public static final String UPDATE_DISH = "Actuslizar plato";
+    public DishWindow dishWindow;
+
+    public HandLingEvents(DishWindow dishWindow){
+
+    }
+}
---
+++ src/run/RunDish.java
@@ -0,0 +1,13 @@
+package run;
+
+import javax.swing.*;
+
+public class RunDish extends JFrame {
+    public DishWindow(){
+        this.setSize(420, 360);
+        this.setTitle("Gestion de Platos");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLayout(null);
+        this.setResizable(true();
+    }
+}
